074c3e985c6171f4e6ace50a9621bca74e4b03e6
Fekade Senbeta <fekadesenbeta@gmail.com>
2021-02-14 22:21:46 -0700

    CS_Calc

---
+++ CS_Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CS_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            string op = Console.ReadLine(); 
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a number: ");
+
+            if (op == "+")
+            {
+                Console.Write(num1 + num2);
+            } else if (op == "-")
+            {
+                Console.Write(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.Write(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.Write(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invaled Operator");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
